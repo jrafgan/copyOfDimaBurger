a60b184675be0d070fcc47b610e6b8fea859816f
Dmitrii Tsyganov <sylvix.dh@gmail.com>
2019-02-04 19:12:59 +0600

    lesson-64 end

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,6 +4,7 @@ import axios from '../../../axios-orders';
 import Button from "../../../components/UI/Button/Button";
 
 import './ContactData.css';
+import Spinner from "../../../components/UI/Spinner/Spinner";
 
 class ContactData extends Component {
 
@@ -11,7 +12,8 @@ class ContactData extends Component {
     name: '',
     email: '',
     street: '',
-    postal: ''
+    postal: '',
+    loading: false
   };
 
   valueChanged = event => {
@@ -28,30 +30,42 @@ class ContactData extends Component {
       customer: {...this.state}
     };
 
+    this.setState({loading: true});
+
     axios.post('orders.json', order).then(() => {
       this.props.history.push('/');
+    }).finally(() => {
+      this.setState({loading: false});
     });
   };
 
   render() {
+    let form = (
+      <form onSubmit={this.orderHandler}>
+        <input className="Input" type="text" name="name" placeholder="Your Name"
+               value={this.state.name} onChange={this.valueChanged}
+        />
+        <input className="Input" type="email" name="email" placeholder="Your Mail"
+               value={this.state.email} onChange={this.valueChanged}
+        />
+        <input className="Input" type="text" name="street" placeholder="Street"
+               value={this.state.street} onChange={this.valueChanged}
+        />
+        <input className="Input" type="text" name="postal" placeholder="Postal Code"
+               value={this.state.postal} onChange={this.valueChanged}
+        />
+        <Button btnType="Success">ORDER</Button>
+      </form>
+    );
+
+    if (this.state.loading) {
+      form = <Spinner />;
+    }
+
     return (
       <div className="ContactData">
         <h4>Enter your Contact Data</h4>
-        <form onSubmit={this.orderHandler}>
-          <input className="Input" type="text" name="name" placeholder="Your Name"
-                 value={this.state.name} onChange={this.valueChanged}
-          />
-          <input className="Input" type="email" name="email" placeholder="Your Mail"
-                 value={this.state.email} onChange={this.valueChanged}
-          />
-          <input className="Input" type="text" name="street" placeholder="Street"
-                 value={this.state.street} onChange={this.valueChanged}
-          />
-          <input className="Input" type="text" name="postal" placeholder="Postal Code"
-                 value={this.state.postal} onChange={this.valueChanged}
-          />
-          <Button btnType="Success">ORDER</Button>
-        </form>
+        {form}
       </div>
     );
   }
